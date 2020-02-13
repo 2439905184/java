3e7d7adde13bad0249f25fc384b0729e8e65bde1
2439905184 <29478722+2439905184@users.noreply.github.com>
2020-02-14 07:35:58 +0800

    Add files via upload

---
+++ mytoast.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class mytoast {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+  public void toast(String str)
+  {
+  System.out.println("成功调用软件内部toast方法");
+  System.out.println("来自Toast的String:"+str);
+  }
+}
